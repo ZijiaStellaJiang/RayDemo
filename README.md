c5f78be12086b8f955a7732337fb47ff1ec5944d
Zijia Jiang <zj68@duke.edu>
2023-06-30 15:10:21 -0400

    basic function done

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ This repo is a demo of distributed Python application. Follow the steps to get s
 Run the following command to install ray package, use `[default]` to ensure all libraries in ray is installed
 
 `pip install 'ray[default]'`
+
+## Run the demo
+Run `python basic_function.py` to compare the run time performance between running locally and with ray.
--- basic_function.py
+++ basic_function.py
@@ -1,17 +1,50 @@
 import ray
 import time
+import os
 
 
 # Ray dashboard: http://localhost:8265
 
-# simple function
-@ray.remote
-def square(n):
-    return n * n
-
 # compute intensive function
+def fibonacci(n):
+    f = []
+    for i in range(0, n):
+        if i < 2:
+            f.append(i)
+            continue
+        f.append(f[i - 1] + f[i - 2])
+    return n
+
+
+# normal function
+def fibonacci_local(n):
+    return fibonacci(n)
+
+
+# ray function, decorated with annotation
+@ray.remote
+def fibonacci_remote(n):
+    return fibonacci(n)
+
+
+# Ray
+def run_remote(sequence_size):
+    # Starting Ray
+    ray.init()
+    start_time = time.time()
+    results = ray.get([fibonacci_remote.remote(sequence_size) for _ in range(os.cpu_count())])
+    duration = time.time() - start_time
+    print('The number to calculate fibonacci: {}, Remote execution time: {}'.format(sequence_size, duration))
+    #ray.shutdown()
+
+
+def run_local(sequence_size):
+    start_time = time.time()
+    results = [fibonacci_local(sequence_size) for _ in range(os.cpu_count())]
+    duration = time.time() - start_time
+    print('The number to calculate fibonacci: {}, Local execution time: {}'.format(sequence_size, duration))
+
 
 if __name__ == '__main__':
-    futures = [square.remote(i) for i in range(200)]
-    results = ray.get(futures)
-    print(results)
+    run_local(100000)
+    run_remote(100000)
